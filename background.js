10252a648faee9904b39bd5eb54a5f189b1b7f8d
Jackie Huang <jackie.huang@mail.utoronto.ca>
2019-12-21 11:23:19 -0500

    Added manifest, background.js and popup file

---
+++ background.js
@@ -0,0 +1,20 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.set({
+        dict: { 0: { term: "Term", definition: "Definition" } },
+        current_question: 0,
+        num_questions: 2
+    }, function () {
+        console.log('Initialized dict.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                // pageUrl: { hostEquals: 'developer.chrome.com' }, Want it to work for all pages so remove.
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
