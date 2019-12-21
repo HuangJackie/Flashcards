5128fd73e5c101a1ea9714be158bf95105da48b8
Jackie Huang <jackie.huang@mail.utoronto.ca>
2019-12-21 16:02:58 -0500

    Added display card

--- popup_menu.js
+++ popup_menu.js
@@ -5,6 +5,9 @@ let term = document.getElementById('term');
 let definition = document.getElementById('definition');
 let confirmation = document.getElementById('confirmation');
 
+let display = document.getElementById('display');
+let result = document.getElementById('result');
+
 submit.onclick = function (element) {
     chrome.storage.local.get('dict', function (data) {
         let length = Object.keys(data.dict).length;
@@ -15,5 +18,17 @@ submit.onclick = function (element) {
             console.log('Saved dict.');
         });
         confirmation.innerHTML = data.dict[length]["term"] + " " + data.dict[length]["definition"];
+    });
+};
+
+display.onclick = function (element) {
+    chrome.storage.local.get('dict', function (data) {
+        let element = document.getElementById('element');
+        if (element.value >= Object.keys(data.dict).length || !/^\d+$/.test(element.value)) {
+            result.innerHTML = "No element";
+        }
+        else {
+            result.innerHTML = data.dict[element.value]["term"] + " " + data.dict[element.value]["definition"];
+        }
     });
 };
